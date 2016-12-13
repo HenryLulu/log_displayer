faa360f5125df53307e553b4f448298e15f3b82c
wanghongyu <why318why@163.com>
2016-12-13 11:43:14 +0800

    add:log4js

--- app.js
+++ app.js
@@ -4,10 +4,26 @@
 var express = require("express");
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var log4js = require('log4js');
 
 var config = require("./common/config");
 var api_router = require("./routers/api_router");
+
+log4js.configure({
+    appenders: [
+        {
+            type: 'file', //文件输出
+            filename: 'logs/access.log',
+            maxLogSize: 1024,
+            backups:3,
+            category: 'normal'
+        }
+    ]
+});
+var logger = log4js.getLogger('normal');
+logger.setLevel('INFO');
+app.use(log4js.connectLogger(logger, {level:log4js.levels.INFO}));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
